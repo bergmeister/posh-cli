3507f60f353db39a442e2fcebb71cd26132229bc
Christoph Bergmeister <c.bergmeister@gmail.com>
2019-08-03 15:38:42 +0100

    First working prototype

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # posh-cli
-A meta module to bootstrap CLI tab completion PowerShell modules
+
+A meta module to bootstrap CLI tab completion PowerShell modules.
+
+# Usage
+
+This is an early prototype with some manual setup action like adding the correct `Import-Module` calls to your `$PROFILE`. At the moment, it analyses the CLIs that you have installed and installs modules from the PSGallery that help with the tab completion.
+
+```pwsh
+git clone https://github.com/bergmeister/posh-cli
+cd posh-cli
+Import-Module .\source\posh-cli.psd1
+Install-TabCompletion
+# Add Import-Module calls to your $PROFILE
+```
---
+++ source/posh-cli.psm1
@@ -0,0 +1,27 @@
+function Install-TabCompletion {
+    [CmdletBinding(SupportsShouldProcess)]
+    param(
+    )
+    
+    # Get available CLIs
+    $applications = (Get-Command -CommandType Application) | ForEach-Object { $_.Name.TrimEnd($_.Extension) }
+    
+    # Store available CLI completion modules remotely so that user's do not have to update this module when checking for updates
+    $completionSourceUri = 'https://raw.githubusercontent.com/bergmeister/posh-cli/master/source/posh-module.json'
+    $completionModules = (Invoke-RestMethod -Uri $completionSourceUri).PSObject.Properties
+    
+    foreach ($completionModule in $completionModules) {
+        $cliName = $completionModule.Name
+        if (-not ($applications.Contains($cliName))) {
+            continue;
+        }
+        $moduleName = $completionModule.Value
+        if (Get-Module -Name $moduleName -ListAvailable) {
+            Write-Verbose "Module '$moduleName' is already installed, skipping"
+            continue;
+        }
+        if ($PSCmdlet.ShouldProcess("Installing module '$moduleName' from PSGallery")) {
+            Install-Module -Name $moduleName -Repository PSGallery -Scope CurrentUser -Force -Verbose
+        }
+    }
+}
