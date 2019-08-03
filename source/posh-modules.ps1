8588768eede90f7dc2965cfca800b7ca5014a1d6
Christoph Bergmeister <c.bergmeister@gmail.com>
2019-08-03 10:32:01 +0100

    Add static list of modules

---
+++ source/posh-modules.ps1
@@ -0,0 +1,12 @@
+@{
+    cargo  = 'posh-cargo'
+    dcos   = 'posh-dcos'
+    dotnet = 'posh-dotnet'
+    docker = 'posh-docker'
+    git    = 'posh-git'
+    mvn    = 'MavenAutoCompletion'
+    npm    = 'npm-completion'
+    scoop  = 'scoop-completion'
+    vsts   = 'posh-vsts-cli'
+    yarn   = 'yarn-completion'
+}
